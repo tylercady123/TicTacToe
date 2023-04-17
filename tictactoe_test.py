4ea9ac002e1a05af91a3ac60998aa46f1d2488b9
tylercady123 <130937849+tylercady123@users.noreply.github.com>
2023-04-16 20:54:20 -0400

    Update tictactoe_test.py

--- tictactoe_test.py
+++ tictactoe_test.py
@@ -1,5 +1,5 @@
 import unittest
-from tictactoe import tic_tac_toe
+from tictactoe import *
 from unittest.mock import patch
 
 class tests(unittest.TestCase):
@@ -7,63 +7,46 @@ class tests(unittest.TestCase):
     def test_cpuMove(self):
         # Test if the CPU's move is a valid integer within the range of the board size.
         size = 3
-        board = tic_tac_toe.createBoard(size)
-        cpu_move = tic_tac_toe.cpuMove(board, size)
-        self.assertTrue(isinstance(cpu_move, int))
-        self.assertTrue(cpu_move >= 0 and cpu_move < size**2)
+        board = [['X', '_', '_'], ['_', 'O', '_'], ['_', '_', '_']]
+        # Ideal CPU move in defined board is in an available corner.
+        availCorners = [(0,2), (2,0), (2,2)]
+        move = cpuMove(board)
+        self.assertIn(move, availCorners)
 
-    def test_userMove(self):
-        # Test if the user's move is a valid integer within the range of the board size.
-        size = 3
-        board = tic_tac_toe.createBoard(size)
-        user_move = tic_tac_toe.userMove(board, size)
-        self.assertTrue(isinstance(user_move, int))
-        self.assertTrue(user_move >= 0 and user_move < size**2)
 
-    def test_checkGameOver(self):
-        # Test if the checkGameOver function correctly identifies a win, tie, or ongoing game.
-        size = 3
-        board = [["X", "O", "O"], ["O", "X", "X"], ["O", "X", "O"]] # board in win state
-        self.assertEqual(tic_tac_toe.checkGameOver(board), "O wins!")
-        board = [["X", "O", "X"], ["X", "O", "O"], ["O", "X", "X"]] # board in tie state
-        self.assertEqual(tic_tac_toe.checkGameOver(board), "Tie game!")
-        board = [["X", "O", "_"], ["X", "O", "_"], ["_", "_", "_"]] # board in ongoing game state
-        self.assertEqual(tic_tac_toe.checkGameOver(board), None)
 
     def test_isWinnable(self):
-        # Test if the isWinnable function correctly identifies a winnable state for the player.
-        board = [["X", "_", "_"], ["O", "X", "_"], ["O", "_", "_"]] # player "X" in winnable state
-        self.assertTrue(tic_tac_toe.isWinnable("X", board))
-        board = [["X", "O", "_"], ["_", "O", "X"], ["_", "_", "X"]] # player "O" in winnable state
-        self.assertTrue(tic_tac_toe.isWinnable("O", board))
-        board = [["X", "O", "X"], ["X", "O", "O"], ["O", "X", "X"]] # no winnable state for either player
-        self.assertFalse(tic_tac_toe.isWinnable("X", board))
-        self.assertFalse(tic_tac_toe.isWinnable("O", board))
+        board1 = [
+            ['X', '_', '_'],
+            ['_', 'O', '_'],
+            ['_', '_', '_']
+        ]
+        board2 = [
+            ['X', 'O', 'X'],
+            ['O', 'O', 'X'],
+            ['_', 'X', 'O']
+        ]
+        # Test if the isWinnable function returns the correct move to win the game.
+        # In this case isWinnable should return True
+        self.assertTrue(isWinnable("X", board1))
+        # In this case isWinnable should return False
+        self.assertFalse(isWinnable("X", board2))
+
 
     def test_getMove(self):
         # Test if the getMove function returns a valid integer within the range of the board size.
         size = 3
-        board = tic_tac_toe.createBoard(size)
-        with patch('builtins.input', return_value="0"):
-            move = tic_tac_toe.getMove(board, size)
-            self.assertTrue(isinstance(move, int))
-            self.assertTrue(move >= 0 and move < size**2)
-        with patch('builtins.input', return_value="9"):
-            move = tic_tac_toe.getMove(board, size)
-            self.assertTrue(isinstance(move, int))
-            self.assertTrue(move >= 0 and move < size**2)
-        with patch('builtins.input', return_value="10"):
-            move = tic_tac_toe.getMove(board, size)
-            self.assertTrue(isinstance(move, int))
-            self.assertTrue(move >= 0 and move < size**2)
+        board = createBoard(size)
+        # Simulate user input (1,1)
+
 
     def test_createBoard(self):
         # Test if the createBoard function returns a board of the correct size.
         size = 3
-        board = tic_tac_toe.createBoard(size)
+        board = createBoard(size)
         self.assertEqual(len(board), size)
-        for row in board:
-            self.assertEqual(len(row), size)
+    
+
     #End of tests
 
 if __name__ == '__main__':
